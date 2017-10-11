c017745511ed716543c4f7dbb3380f8a10beea7f
sheridanvk <sheridan.kates@gmail.com>
2017-10-11 13:30:26 +0100

    Add POST /todos route and refactor mongoose

---
+++ server/models/user.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
+});
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = {User};
